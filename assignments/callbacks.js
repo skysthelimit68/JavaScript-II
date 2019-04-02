e2ccf5bc6035e81ee80f023922dc1a72d39e218e
Ming Liu <mliu@macpro.home>
2019-04-02 14:08:09 -0400

    started with some stretch problems

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,29 +27,56 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, cb = (elem) => console.log(elem));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+last(items, cb = (elem) => console.log(elem));
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let result = x+y;
+  return cb(result);
 }
+sumNums(3, 5, cb = (elem) => console.log(elem));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let result = x*y;
+  return cb(result);
 }
+multiplyNums(3, 5, cb = (elem) => console.log(elem));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
+  let result = list.includes(item)? true: false;
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(result);
 }
+contains("Pencil", items, cb = (elem) => console.log(elem));
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
+  let uniq = [];
+  //array.filter(elem => (!uniq.includes(elem)));
+  for(let i = 0; i < array.length; i++) {
+    if(!uniq.includes(array[i])) {
+      uniq.push(array[i]);
+    }
+  }
+  //console.log(uniq);
   // Pass the duplicate free array to the callback function.
+  cb(uniq);
+
   // Do not mutate the original array.
 }
+let array_1 = [1,3,2,3,3,322,2,5,2,4,698,29,9];
+
+removeDuplicates(array_1, cb = (array => console.log(array)));
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,15 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function houseOfDogs() {
+  const original = "my original six pups";
+  console.log(`I miss ${original} :(`);
+  function dogsNow() {
+    const badBeagles = "my two beagles";
+    console.log(`Why can't ${badBeagles} behave like ${original}?`);
+  } 
+  dogsNow();
+}
+houseOfDogs();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,14 +17,31 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter;
+  return counter++;
 };
+
+const newCounter = counter();
+//newCounter();
+//newCounter();
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  obj = {
+    counter:0,
+    increment: function(){ this.counter++; console.log(this.counter);},
+    decrement: function(){ this.counter--; console.log(this.counter);}
+  }
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+counterFactory();
+console.log(obj.counter);
+obj.increment();
+obj.increment();
+obj.decrement();
+
